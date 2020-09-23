8ff33910d68038251bdd298ed8aa3a54e744347b
thalm <thalmann.arne@gmail.com>
2020-09-23 22:17:45 +0200

    git test

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -23,14 +23,7 @@ class login extends Component {
         {token: 'abc'}
       }
 
-/*
-      componentDidMount() {
-        // Simple POST request with a JSON body using axios
-        const article = { title: 'React POST Request Example' };
-        axios.post('https://reqres.in/api/articles', article)
-            .then(response => this.setState({ articleId: response.data.id }));
-    }
-*/
+
 
 
 
