be569838695e47bbb0f82977e3cb547b07574759
David Angeles Ungson <davidangelesu@gmail.com>
2020-10-21 23:16:26 +0200

    Added Redux Store to project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const authReducer = import("./store/reducers/auth");
+//*** setup Redux Store
+//development settup
+const composeEnhancers =
+	process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+//reducers
+const rootReducer = combineReducers({
+	auth: authReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
-
-  <BrowserRouter> <React.StrictMode>
-  <App />
-</React.StrictMode></BrowserRouter>
-)
-
-ReactDOM.render(app
- ,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
