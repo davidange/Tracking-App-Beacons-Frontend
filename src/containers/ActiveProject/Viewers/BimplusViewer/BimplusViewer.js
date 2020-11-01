7869d63e0bbca536c345e2e9fc5e3d34a1384ed4
David Angeles Ungson <davidangelesu@gmail.com>
2020-11-01 14:42:23 +0100

    Update BimplusViewer.js

--- src/containers/ActiveProject/Viewers/BimplusViewer/BimplusViewer.js
+++ src/containers/ActiveProject/Viewers/BimplusViewer/BimplusViewer.js
@@ -8,7 +8,7 @@ const BimplusViewer = (props) => {
 
 	const renderer = (
 		<iframe
-			title="Renderer"
+			title="Viewer"
 			id="bimplusExplorer"
 			style={{ border: 0, height: "100%", width: "100%", backgroundColor: "white", margin: 0, padding: 0 }}
 		></iframe>
